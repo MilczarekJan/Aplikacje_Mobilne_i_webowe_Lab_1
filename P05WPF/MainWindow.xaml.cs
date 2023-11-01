34bc0357244acd0a341f0c4a8ecbf5961f975581
MilczarekJan <101673349+MilczarekJan@users.noreply.github.com>
2023-11-01 19:55:56 +0100

    Postepy 01.11.2023

---
+++ P05WPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace P05Shop.Client
+{
+    public partial class MainWindow : Window
+    {
+        public ShoeViewModel ViewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new ShoeViewModel();
+            DataContext = ViewModel;
+        }
+
+        private async void GetShoe_Click(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.GetShoe();
+        }
+
+        private async void AddShoe_Click(object sender, RoutedEventArgs e)
+        {
+            var name = NameTextBox.Text;
+            var description = DescriptionTextBox.Text;
+            await ViewModel.AddShoe(name, description);
+        }
+
+        private async void DeleteShoe_Click(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.DeleteShoe();
+        }
+
+        private async void UpdateShoe_Click(object sender, RoutedEventArgs e)
+        {
+            var name = NameTextBox.Text;
+            var description = DescriptionTextBox.Text;
+            await ViewModel.UpdateShoe(name, description);
+        }
+    }
+}
---
+++ P05WPF/ViewModels/ShoeViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using P06Shop.Shared.Shop;
+
+public class ShoeViewModel : INotifyPropertyChanged
+{
+    private string apiBaseUrl = "http://localhost:5093/";
+    private ApiClient apiClient;
+    private int selectedShoeId;
+
+    public int SelectedShoeId
+    {
+        get { return selectedShoeId; }
+        set
+        {
+            selectedShoeId = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public List<Shoe> Shoes { get; set; } // You can bind this to a UI element
+
+    public Shoe SelectedShoe { get; set; } // You can bind this to a UI element
+
+    public ShoeViewModel()
+    {
+        apiClient = new ApiClient(apiBaseUrl);
+        LoadShoes();
+    }
+
+    public async Task LoadShoes()
+    {
+        var result = await apiClient.GetShoesAsync();
+        Shoes = result.Data;
+        OnPropertyChanged(nameof(Shoes));
+    }
+
+    public async Task GetShoe()
+    {
+        if (SelectedShoeId > 0)
+        {
+            var result = await apiClient.GetShoeAsync(SelectedShoeId);
+            SelectedShoe = result.Data;
+            OnPropertyChanged(nameof(SelectedShoe));
+        }
+    }
+
+    public async Task AddShoe(string name, string description)
+    {
+        var newShoe = new Shoe { Name = name, Description = description };
+        await apiClient.AddShoeAsync(newShoe);
+        LoadShoes();
+    }
+
+    public async Task DeleteShoe()
+    {
+        if (SelectedShoeId > 0)
+        {
+            var result = await apiClient.DeleteShoeAsync(SelectedShoeId);
+            Console.WriteLine(result);
+            LoadShoes();
+        }
+    }
+
+    public async Task UpdateShoe(string name, string description)
+    {
+        if (SelectedShoeId > 0)
+        {
+            var updatedShoe = new Shoe { Name = name, Description = description };
+            await apiClient.UpdateShoeAsync(SelectedShoeId, updatedShoe);
+            LoadShoes();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
