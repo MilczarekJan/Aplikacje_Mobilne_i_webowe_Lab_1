1a5f53a87d10fee935d67a32e06eaa3488f98f07
MilczarekJan <101673349+MilczarekJan@users.noreply.github.com>
2023-11-17 22:32:34 +0100

    Shoe Add dziala

--- P09BlazorApp/Models/AddShoeDto.cs
+++ P09BlazorApp/Models/AddShoeDto.cs
@@ -5,5 +5,11 @@
 		public double ShoeSize { get; set; }
 		public string Description { get; set; }
 		public string Name { get; set; }
+
+		public AddShoeDto(double size, string description, string name) { 
+			this.Description = description;
+			this.Name = name;
+			this.ShoeSize = size;
+		}
 	}
 }
